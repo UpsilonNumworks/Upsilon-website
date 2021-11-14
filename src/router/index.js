1f8a233e0b39a8bad3bdf465beaad92d3c4ab1ca
lolocomotive <lolocomotive181@gmail.com>
2021-11-14 14:08:45 +0100

    Added RPN documentation

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Latex from '../views/Latex.vue'
 import Installer from '../views/Installer.vue'
+import RPN from '../views/RPN.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/install',
     name: 'Install',
     component: Installer
+  },
+  {
+    path: '/rpn',
+    name: 'RPN',
+    component: RPN
   }
 ]
 
---
+++ src/views/RPN.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="rpn-doc">
+    <h1>RPN</h1>
+    <p>
+      The <em>Reverse Polish Notation</em> is another way of writing
+      mathematical calculations. It was invented by the polish mathematician Jan
+      Łukasiewicz in 1924.
+    </p>
+    <p>
+      This notation provides several advantages : RPN calculators are based on
+      the use of a stack, the operands are placed on top of the stack, while the
+      results <br />
+      of the calculations are also returned on top of the stack. Although this
+      might be disconcerting at first, one quickly realizes that it helps make
+      mathematical expressions <br />
+      easier to understand and manipulate.
+    </p>
+    <h2>Quick introduction to the RPN app on Upsilon.</h2>
+    <p>
+      When you want to do a calculation in the RPN app on Upsilon, you must
+      start by writing all the numbers that will be involved in your expression
+      in order of appearance. Then you press the appropriate operand also in
+      order of apparition.
+    </p>
+    <p>Examples :</p>
+    To obtain the result of 245*6598 :<br />
+    <ol>
+      <li>Write 245 and press OK</li>
+      <li>Write 6598 and press OK</li>
+      <li>Press once the multiplication button</li>
+    </ol>
+    <p>To obtain the result of (65*98)/78 :</p>
+    <ol>
+      <li>Write 65 and press OK</li>
+      <li>Write 98 and press OK</li>
+      <li>Write 78 and press OK</li>
+      <li>Press once the multiplication button</li>
+      <li>Press once the division button</li>
+    </ol>
+    <p>
+      You can change the position of a term in the stack by using the
+      parenthesis button, which will move it up or down.
+    </p>
+    <p>
+      <i
+        ><b>Note</b> : throughout the whole Omega history, a bug made the RPN
+        app hard to safely use on the calculator, however, thanks to RedGlow,
+        dl11 and Overengined, the bug was fixed, and i is now totally safe to
+        use the RPN app.</i
+      >
+    </p>
+    <dl>
+      <dt><br /></dt>
+    </dl>
+  </div>
+</template>
