ca16c286bc86ac48b8041764c5700a908db83bcb
lolocomotive <lolocomotive181@gmail.com>
2022-02-01 18:24:29 +0100

    Added custom select element to be able to style it better later

---
+++ src/components/CustomSelect.vue
@@ -0,0 +1,25 @@
+<template>
+  <select :name="sid" :id="sid">
+    <slot></slot>
+  </select>
+</template>
+<script>
+export default {
+  props: {
+    sid: String
+  }
+}
+</script>
+<style scoped>
+select {
+  border: solid var(--upsilon-1) 2pt;
+  border-radius: 3px;
+  background-color: var(--upsilon-2);
+  color: var(--foreground);
+  display: block;
+  margin: 0.5em;
+  font-size: 1em;
+  padding: 0.5em;
+  text-align: center;
+}
+</style>
