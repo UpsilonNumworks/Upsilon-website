55bad2e7c4cd561898055a521020e222102d58af
lolocomotive <lolocomotive181@gmail.com>
2022-04-12 16:42:21 +0200

    [Releases] Cleanup HTML

---
+++ src/components/ReleaseItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="release-item">
+    <div :class="'release-tag release-tag-' + (chg ? 'chg' : 'new')">
+      {{ chg ? "CHG" : "NEW" }}
+    </div>
+    <div><slot></slot></div>
+  </div>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n'
+export default {
+  name: 'ReleaseItem',
+  props: {
+    chg: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup () {
+    const { t } = useI18n({
+      inheritLocale: true,
+      useScope: 'global'
+    })
+    return { t }
+  }
+}
+</script>
+
+<style scoped>
+.release-tag {
+  width: 40px;
+  text-align: center;
+  font-size: 14px;
+  line-height: 26px;
+  font-weight: 600;
+  color: var(--foreground);
+  border-radius: 6px;
+  padding: 0 4px;
+  user-select: none;
+  outline-offset: -2pt;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin:4px;
+}
+
+.release-tag-new {
+  background: var(--tag-new);
+  outline: solid var(--tag-new-outline) 2pt;
+}
+.release-tag-chg {
+  background: var(--tag-change);
+  outline: solid var(--tag-change-outline) 2pt;
+}
+.release-item{
+    display:flex;
+    align-items: center;
+    margin:4px;
+}
+</style>
--- src/views/Releases.vue
+++ src/views/Releases.vue
@@ -5,69 +5,25 @@
       <div class="generic-page release-table">
         <p>{{ t('releases.version') }} 1.0 ({{ t('releases.dev') }})</p>
         <div class="grid">
-          <!--TODO clean this up-->
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.latex') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.E16Security') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.RecoveryMode') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.Background') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.Themes') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.Ulab') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.PythonBattery') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.PythonSettingsAutocomplete') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.KandinskyEnhancements') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.Reader') }}</div>
-
-          <div class="release-tag release-tag-change">
-            {{ t('releases.change') }}
-          </div>
-          <div>{{ t('releases.versions.1-0.PythonBaseScripts') }}</div>
-
-          <div class="release-tag release-tag-change">
-            {{ t('releases.change') }}
-          </div>
-          <div>{{ t('releases.versions.1-0.StorageSize') }}</div>
-
-          <div class="release-tag release-tag-change">
-            {{ t('releases.change') }}
-          </div>
-          <div>
+          <ReleaseItem>{{ t('releases.versions.1-0.latex') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.E16Security') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.RecoveryMode') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.Background') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.Themes') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.Ulab') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.PythonBattery') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.PythonSettingsAutocomplete') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.KandinskyEnhancements') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.Reader') }}</ReleaseItem>
+          <ReleaseItem chg>{{ t('releases.versions.1-0.PythonBaseScripts') }}</ReleaseItem>
+          <ReleaseItem chg>{{ t('releases.versions.1-0.StorageSize') }}</ReleaseItem>
+          <ReleaseItem chg>
             {{ t('releases.versions.1-0.VariableLoadedPythonToolbox') }}
-          </div>
-
-          <div class="release-tag release-tag-change">
-            {{ t('releases.change') }}
-          </div>
-          <div>{{ t('releases.versions.1-0.BatteryLogo') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.BatteryPercentage') }}</div>
-
-          <div class="release-tag release-tag-change">
-            {{ t('releases.change') }}
-          </div>
-          <div>{{ t('releases.versions.1-0.EqualSymplification') }}</div>
-
-          <div class="release-tag release-tag-new">{{ t('releases.new') }}</div>
-          <div>{{ t('releases.versions.1-0.SecondDegreePolynominal') }}</div>
+          </ReleaseItem>
+          <ReleaseItem chg>{{ t('releases.versions.1-0.BatteryLogo') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.BatteryPercentage') }}</ReleaseItem>
+          <ReleaseItem chg>{{ t('releases.versions.1-0.EqualSymplification') }}</ReleaseItem>
+          <ReleaseItem>{{ t('releases.versions.1-0.SecondDegreePolynominal') }}</ReleaseItem>
         </div>
       </div>
     </div>
@@ -75,8 +31,12 @@
 </template>
 <script>
 import { useI18n } from 'vue-i18n'
+import ReleaseItem from '@/components/ReleaseItem.vue'
 export default {
   name: 'ReleasesPage',
+  components: {
+    ReleaseItem
+  },
   setup () {
     const { t } = useI18n({
       inheritLocale: true,
@@ -88,47 +48,7 @@ export default {
 </script>
 
 <style scoped>
-.grid {
-  display: grid;
-  grid-template-columns: max-content auto;
-}
-.grid > * {
-  margin: 5px;
-}
-.release-table {
-  margin-top: 30px;
-  padding: 2em;
-}
-.release-table > p {
-  margin-top: 0;
-}
-.release-tag {
-  width: 40px;
-  text-align: center;
-  font-size: 14px;
-  line-height: 26px;
-  font-weight: 600;
-  color: var(--foreground);
-  border-radius: 6px;
-  padding: 0 4px;
-  user-select: none;
-  outline-offset: -2pt;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 
-.release-tag-new {
-  background: var(--tag-new);
-  outline: solid var(--tag-new-outline) 2pt;
-}
-.release-tag-change {
-  background: var(--tag-change);
-  outline: solid var(--tag-change-outline) 2pt;
-}
-.release-tag-update {
-  background: var(--tag-update);
-}
 #releases {
   display: flex;
   justify-content: center;
